f30bb958ef367adcf42f536fbeaff1fe1f99f8d8
Michael <mnath10@stuy.edu>
2019-05-25 08:47:08 -0400

    Created rudimentary site that yields house results from CSV

--- bulk.py
+++ bulk.py
@@ -1,14 +1,19 @@
-#! usr/bin/python3
+#! /usr/bin/python3
 import cgi
 import cgitb
 # Note to Self: Make sure to convert any numbers back to it's respective comma form
-cgittb.enable()
+cgitb.enable()
 print('Content-type: text/html\n')
+form = cgi.FieldStorage()
 def main():
     empty = {}
     data = open("houses.csv", "r").read().split("\n")
     houses = data[1:]
     for i in houses:
         empty[i.split(",")[0]] = i.split(",")[1:]
+    print("<html>")
     for key, value in empty.items():
-        print(key, value)
+        print(key, value, "<br>")
+    print("</html")
+if form.getvalue("Press_me") == "Test":
+    main()
