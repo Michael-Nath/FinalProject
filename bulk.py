33dae45a6fd07151743761996a2a6b2fd91c6cdd
Michael <mnath10@stuy.edu>
2019-05-31 19:28:36 -0400

    Commit before I made big error

--- bulk.py
+++ bulk.py
@@ -6,16 +6,35 @@ cgitb.enable()
 print('Content-type: text/html\n')
 form = cgi.FieldStorage()
 def main():
-    answers = [form.getvalue("Locations"), form.getvalue("Beds"), form.getvalue("Baths"), form.getvalue("PriceRange")]
+    answers = [form.getvalue("Locations"), form.getvalue("Type"), form.getvalue("Beds"), form.getvalue("Baths")]
+    anys = []
+    true_answers = []
+    checked = False
+    for i in range(len(answers)):
+        anys.append(answers[i] if answers[i] == "Any" else true_answers.append(answers[i]))
+
     empty = {}
     data = open("Sheet1.csv", "r").read().split("\n")
-    houses = data[1:]
-    for i in houses:
+    data = data[1:]
+    for i in data:
         empty[i.split(",")[0]] = i.split(",")[1:]
+
     print("<html>")
-    for key, value in empty.items():
-        if all(x in value for x in answers if x != "Any"):
+    print(answers)
+
+    if true_answers == []:
+        for key, value in empty.items():
             print(key, value, "<br>")
+            checked = True
+    else:
+        for key, value in empty.items():
+            if all(x in value for x in true_answers):
+                print(key, value, "<br>")
+                checked = True
+
+    if checked == False:
+        print("You are either highly unrealistic or terribily realistic... Please go back and refine your filters. ")
+
     print("</html")
 
 if form.getvalue("Press_me") == "Test":
