d620c283317e045497b95ddd2c6343271346058e
Michael <mnath10@stuy.edu>
2019-06-02 21:07:32 -0400

    Changed some stuff

--- bulk.py
+++ bulk.py
@@ -1,15 +1,35 @@
 #! /usr/bin/python3
 import cgi
 import cgitb
+
 # Note to Self: Make sure to convert any numbers back to it's respective comma form
 cgitb.enable()
 print('Content-type: text/html\n')
 form = cgi.FieldStorage()
+
+def intersection(lst1, lst2):
+    if lst1 == []:
+        if lst2 != []:
+            return lst2
+        else:
+            return []
+    if lst2 == []:
+        if lst1 != []:
+            return lst1
+        else:
+            return []
+
+    lst3 = [value for value in lst1 if value in lst2]
+    return lst3
+
 def main():
+    Location = []
+    Type = []
+    Beds = []
+    Baths = []
     answers = [form.getvalue("Locations"), form.getvalue("Type"), form.getvalue("Beds"), form.getvalue("Baths")]
     anys = []
     true_answers = []
-    checked = False
     for i in range(len(answers)):
         anys.append(answers[i] if answers[i] == "Any" else true_answers.append(answers[i]))
 
@@ -20,22 +40,35 @@ def main():
         empty[i.split(",")[0]] = i.split(",")[1:]
 
     print("<html>")
-    print(answers)
-
     if true_answers == []:
         for key, value in empty.items():
             print(key, value, "<br>")
-            checked = True
     else:
-        for key, value in empty.items():
-            if all(x in value for x in true_answers):
-                print(key, value, "<br>")
-                checked = True
+        def helper(List, answer):
+            for key, value in empty.items():
+                if type(answer) == list:
+                    if any(x in value for x in answer):
+                        List.append([key, value])
+                else:
+                    if answer in value:
+                        List.append([key, value])
 
-    if checked == False:
+    if form.getvalue("Locations") != "Any":
+        helper(Location, form.getvalue("Locations"))
+    if form.getvalue("Type") != "Any":
+        helper(Type, form.getvalue("Type"))
+    if form.getvalue("Beds") != "Any":
+        helper(Beds, form.getvalue("Beds"))
+    if form.getvalue("Baths") != "Any":
+        helper(Baths, form.getvalue("Baths"))
+
+    intersected = intersection(intersection(Location, Type),intersection(Beds, Baths))
+    if intersected == [] and true_answers != []:
         print("You are either highly unrealistic or terribily realistic... Please go back and refine your filters. ")
-
+    else:
+        print(intersected)
     print("</html")
+
 
 if form.getvalue("Press_me") == "Test":
     main()
